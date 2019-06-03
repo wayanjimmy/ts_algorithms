245cfa983ee66aa752d2ef353d376f61c8b129cf
Wayan Jimmy <jimmyeatcrab@gmail.com>
2019-06-03 14:16:42 +0700

    Binary search

---
+++ algorithms/binary_search_test.ts
@@ -0,0 +1,15 @@
+import { test } from "https://deno.land/std@v0.7/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std@v0.7/testing/asserts.ts";
+import { binarySearch } from "./binary_search.ts";
+
+test({
+  name: "[binary search] should return true",
+  fn: () =>
+    assertEquals(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 56), true)
+});
+
+test({
+  name: "[binary search] should return false",
+  fn: () =>
+    assertEquals(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 72), false)
+});
--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
 import { runTests } from "https://deno.land/std@v0.7/testing/mod.ts";
 import "./algorithms/fizz_buzz_test.ts";
+import "./algorithms/binary_search_test.ts";
 import "./data_structures/singly_linked_list_test.ts";
 
 runTests();
